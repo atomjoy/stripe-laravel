e9ae69dcffff7b5c51162d5f4884dc163aec13b1
Atomjoy <atomjoy.official@gmail.com>
2024-05-12 18:33:05 +0200

    Add files via upload

---
+++ .vscode/extensions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get installed extensions list
+# code --list-extensions | % { "code --install-extension $_" }
+
+# List
+code --install-extension ahinkle.laravel-model-snippets
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
+code --install-extension azemoh.one-monokai
+code --install-extension beardedbear.beardedtheme
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension cnmorocho.min-gruvbox-theme
+code --install-extension codingyu.laravel-goto-view
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dotjoshjohnson.xml
+code --install-extension esbenp.prettier-vscode
+code --install-extension github.github-vscode-theme
+code --install-extension liamhammett.inline-parameters
+code --install-extension lninl.theme-hbuilderx-soft-green-light
+code --install-extension moldaz.minimal-green
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension onecentlin.laravel-blade
+code --install-extension pkief.material-icon-theme
+code --install-extension sharmil.dark-room
+code --install-extension shufo.vscode-blade-formatter
+code --install-extension vue.volar
+code --install-extension wheredoesyourmindgo.gruvbox-concoctis
+code --install-extension xabikos.javascriptsnippets
